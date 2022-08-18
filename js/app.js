1a7322a1d2368d3f193d9c4325a0e095eeb0295a
davidobado023 <51761002+davidobado023@users.noreply.github.com>
2022-08-18 21:16:00 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,21 +2,22 @@ $(document).ready(function(){
     //HERO SLIDER
     $('#hero-slider').owlCarousel({
         loop:true,
+        autoplay:true,
+        autoplayHoverPause:true,
         margin:0,
         nav:true,
         items: 1,
         dots: false,
         smartSpeed: 1000,
-        responsive:{
-            0:{
-                
-            },
-            600:{
-                
-            },
-            1000:{
-                
-            }
-        }
     })
 });
+
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+    })
+
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+    })
+
+
