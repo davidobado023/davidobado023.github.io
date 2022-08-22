81af3c3a6ae285a790f5fec09b33ee270d07f4d2
davidobado023 <davidobado0023@gmail.com>
2022-08-22 20:41:54 +0800

    Update

--- js/app.js
+++ js/app.js
@@ -35,14 +35,21 @@ $(document).ready(function(){
         nav: false,
         dots: true,
         smartSpeed: 600,
+        margin: 24,
         autoplay: true,
         autoplayTimeout: 4000,
         responsive:{
             0:{
                 items:1
             },
-            600:{
+            768:{
+                items:2
+            },
+            1024:{
                 items:3
+            },
+            1440:{
+                items:4
             },
         }
     })
@@ -55,6 +62,69 @@ $('.play').on('click',function(){
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
     })
+
+//HomepageContact
+function validation(){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    if(!name.match(/^[a-zA-Z ]*$/)){
+        error.innerHTML = 'Invalid name or email address';
+        return false;
+    } 
+    if (!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        error.innerHTML = 'Invalid name or email address';
+        return false;
+    }
+    return true;
+}
+
+var error = document.getElementById('error')
+
+function checkStatus(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    if(name.length ==0){
+        error.innerHTML = '';
+    }
+    if(email.length ==0){
+        error.innerHTML = '';
+    }
+}
+
+
+
+//ModalContact
+function validationModal(){
+    const nameModal = document.getElementById('nameModal').value;
+    const emailModal = document.getElementById('emailModal').value;
+
+    if(!nameModal.match(/^[a-zA-Z ]*$/)){
+        errorModal.innerHTML = 'Invalid name or email address';
+        return false;
+    } 
+    if (!emailModal.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        errorModal.innerHTML = 'Invalid name or email address';
+        return false;
+    }
+    return true;
+}
+var errorModal = document.getElementById('errorModal')
+
+function checkStatusModal(){
+    var nameModal = document.getElementById('nameModal').value;
+    var emailModal = document.getElementById('emailModal').value;
+
+    if(nameModal.length ==0){
+        errorModal.innerHTML = '';
+    }
+    if(emailModal.length ==0){
+        errorModal.innerHTML = '';
+    }
+}
+
+
 
     
 
